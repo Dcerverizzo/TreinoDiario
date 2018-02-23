35f8c91f177ece1ed936d20db96a882fea926bd9
Daniel Cerverizzo <danielcerverizzo@gmail.com>
2018-02-23 11:49:21 -0300

    Trabalhando com arrays Intermediarios

---
+++ Object Oriented and Functional Programming/Join Strings with join.js
@@ -0,0 +1,8 @@
+/*Use the join method to create a string from joinMe with spaces in between each element and assign it to joinedString. */
+
+var joinMe = ["Split","me","into","an","array"];
+var joinedString = '';
+
+// Only change code below this line.
+
+joinedString = joinMe.join(" ");
